2784ff5859a5372d2388b9a12a735413b0dbac2c
Trung Tran <trannamtrung1st@gmail.com>
2023-12-13 22:27:18 +0700

    Add routing

---
+++ Tutorials/RabbitMQLearning.Routing.EmitLogDirect/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+var properties = channel.CreateBasicProperties();
+properties.Persistent = true;
+channel.ExchangeDeclare("direct_logs", ExchangeType.Direct);
+
+string? message;
+do
+{
+    Console.Write("Input your message (or leave empty to exit): ");
+    message = Console.ReadLine();
+    if (string.IsNullOrEmpty(message)) break;
+    Console.Write("Input color to direct: ");
+    var color = Console.ReadLine();
+
+    var body = Encoding.UTF8.GetBytes(message);
+    channel.BasicPublish(exchange: "direct_logs",
+                         routingKey: color,
+                         basicProperties: null,
+                         body: body);
+} while (!string.IsNullOrEmpty(message));
---
+++ Tutorials/RabbitMQLearning.Routing.ReceiveLogsDirect/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+var queueName = channel.QueueDeclare().QueueName;
+
+Console.Write("Enter console color (white if blank): ");
+var color = Console.ReadLine();
+if (!string.IsNullOrWhiteSpace(color))
+{
+    Console.ForegroundColor = Enum.Parse<ConsoleColor>(color);
+}
+channel.QueueBind(queue: queueName,
+                  exchange: "direct_logs",
+                  routingKey: color);
+
+// If all the workers are busy, your queue can fill up. You will want to keep an eye on that, and maybe add more workers, or have some other strategy.
+channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+Console.WriteLine(" [*] Waiting for messages.");
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] Received {message}");
+};
+
+channel.BasicConsume(queue: queueName,
+                     autoAck: true,
+                     consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
