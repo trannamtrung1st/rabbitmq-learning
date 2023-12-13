3381e4fa88e4e8e48a2966ce4d60fefa520c6a0f
Trung Tran <trannamtrung1st@gmail.com>
2023-12-13 22:00:54 +0700

    Add Pubsub

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## Utils
 
 + `sudo rabbitmqctl list_queues name messages_ready messages_unacknowledged`
++ `sudo rabbitmqctl list_exchanges`
++ `rabbitmqctl list_bindings`
---
+++ Tutorials/RabbitMQLearning.PubSub.EmitLog/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+var properties = channel.CreateBasicProperties();
+properties.Persistent = true;
+channel.ExchangeDeclare("logs", ExchangeType.Fanout);
+
+string? message;
+do
+{
+    Console.Write("Input your message (or leave empty to exit): ");
+    message = Console.ReadLine();
+    if (string.IsNullOrEmpty(message)) break;
+
+    var body = Encoding.UTF8.GetBytes(message);
+    channel.BasicPublish(exchange: "logs",
+                         routingKey: string.Empty,
+                         basicProperties: null,
+                         body: body);
+} while (!string.IsNullOrEmpty(message));
