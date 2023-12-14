97ea3567da882005dcece54178ebd159d92f2c3c
Trung Tran <trannamtrung1st@gmail.com>
2023-12-14 13:55:26 +0700

    Add topics

---
+++ Tutorials/RabbitMQLearning.Topics.EmitLogTopics/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+var properties = channel.CreateBasicProperties();
+properties.Persistent = true;
+channel.ExchangeDeclare("topic_logs", ExchangeType.Topic);
+
+string? message;
+do
+{
+    Console.Write("Input your message (or leave empty to exit): ");
+    message = Console.ReadLine();
+    if (string.IsNullOrEmpty(message)) break;
+    Console.Write("Input topic: ");
+    var topic = Console.ReadLine();
+
+    var body = Encoding.UTF8.GetBytes(message);
+    channel.BasicPublish(exchange: "topic_logs",
+                         routingKey: topic,
+                         basicProperties: null,
+                         body: body);
+} while (!string.IsNullOrEmpty(message));
---
+++ Tutorials/RabbitMQLearning.Topics.ReceiveLogsTopics/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+var queueName = channel.QueueDeclare().QueueName;
+
+Console.Write("Enter topic: ");
+var topic = Console.ReadLine();
+
+channel.QueueBind(queue: queueName,
+                  exchange: "topic_logs",
+                  routingKey: topic);
+
+// If all the workers are busy, your queue can fill up. You will want to keep an eye on that, and maybe add more workers, or have some other strategy.
+channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+Console.WriteLine(" [*] Waiting for messages.");
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] Received {message}");
+};
+
+channel.BasicConsume(queue: queueName,
+                     autoAck: true,
+                     consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
